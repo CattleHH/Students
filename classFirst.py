1180a01b05fc79dd7dc4f0011e7e57bd33cd16fd
CattleHH <jiayaqing1989@163.com>
2018-11-23 14:22:04 +0800

    CattleHH

---
+++ classFirst.py
@@ -0,0 +1,17 @@
+class Student():
+    pass
+mingyue=Student()
+
+class PythonStudent():
+    name=None
+    age=18
+    course="python"
+    def doHomeWork(self):
+        print("做作业")
+        return None
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.course)
+yueyue.doHomeWork()
+
+
